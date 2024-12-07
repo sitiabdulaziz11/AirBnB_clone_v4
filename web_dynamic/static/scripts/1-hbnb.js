59b5e466e7017f8ae0668a80945ad531f9701b44
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-12-07 16:38:55 +0300

    task 2 of Airbnb 6

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,34 @@
-$(doument).ready(function(){
-    const selectedAmenities = [];
+$(document).ready(function(){
+    let selectedAmenities = [];
     // const selectedAmenities = {};
 
     // Listen for changes on each checkbox
     $('input[type="checkbox"]').change(function(){
-        const amenityId = $(this).attr('data-id');
-        const amenityName = $(this).attr('data-name');
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+        // const amenityId = $(this).attr('id');
+        // const amenityName = $(this).attr('name');
 
         if ($(this).is(':checked')) {
             // Add amenity ID and name to the list
-            selectedAmenities[amenityId] = amenityName;
+            // selectedAmenities[amenityId] = amenityName;
+            selectedAmenities.push({id: amenityId, name: amenityName});
+            // console.log(`Added: ${amenityName} (ID: ${amenityId})`);
+
         } else {
             // Remove amenity ID from the list
-            delete selectedAmenities[amenityId];
+            // delete selectedAmenities[amenityId];
+            selectedAmenities = selectedAmenities.filter((amenity) => amenity.id !== amenityId);
+            // console.log(`Removed: ${amenityName} (ID: ${amenityId})`);
         }
 
         // Update the h4 tag with the list of selected amenities
-        const amenityList = Object.values(selectedAmenities).join(', ');
-        $('.Amenities h4').text(amenityList);
+        // const amenityList = Object.values(selectedAmenities).join(', ');
+        const amenityList = selectedAmenities.map(amenity => amenity.name).join(', ');
+        // $('.Amenities h4').text(amenityList);
+        $('#selected-amenities').text(amenityList);
+
+        // Log the current list of selected amenities
+        // console.log('Current Selected Amenities:', selectedAmenities);
     });
 });
